3313a94c3e18d351819ce8b421e59e4dc5b804f2
Matt Laux <matthew.laux@gmail.com>
2015-10-01 01:20:30 -0500

    sanitize usernames

--- centrifuge.py
+++ centrifuge.py
@@ -2,6 +2,7 @@ from flask import Flask, abort, g, jsonify, request
 app = Flask(__name__)
 
 import sqlite
+import re
 from crossdomain import crossdomain
 
 SELECT_ALL_HISCORES = 'select * from hiscores order by score desc limit ?, ?'
@@ -34,8 +35,13 @@ def put_hiscore():
   if not 'username' in request.json or not 'score' in request.json:
     abort(400)
 
-  sqlite.query_db(INSERT_NEW_HISCORE, (request.json.get('username'),
-                                       request.json.get('score')))
+  username = str(request.json.get('username'))
+  username = username.lower()
+  username = re.sub(r'[^a-z]', '', username)
+  if len(username) > 12:
+    username = username[:12]
+
+  sqlite.query_db(INSERT_NEW_HISCORE, (username, request.json.get('score')))
   return jsonify({'result': 'success'})
 
 @app.route('/centrifuge/api/errors', methods=['PUT'])
@@ -49,5 +55,5 @@ def put_error():
   return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0', debug=True)
 
